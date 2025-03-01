ccd70389a4d0bbacf23061c4346087adebf81172
Arzymat01 <arzymatarkabaev01@gmail.com>
2025-03-01 10:35:35 +0600

    27 day

---
+++ backend/users/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return {"user": user}
+        raise serializers.ValidationError("Invalid credentials")
