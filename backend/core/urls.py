90d64552534bd756203d35de70e80488e16d73f3
Arzymat01 <arzymatarkabaev01@gmail.com>
2025-03-02 10:40:06 +0600

    28 day

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Welcome to Doctor Consultant API!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),  # Бул сап users' urls.py файлын кошуш керек
+    path('api/', include('users.urls')),
+    path('', home),  # Бул линия башкы маршрут үчүн
 ]
